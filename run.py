2bf5c7d0f438387d6024f06d57006d6efb86187e
零度冷咖啡 <sviptzk@163.com>
2020-05-15 21:58:54 +0800

    修改

--- run.py
+++ run.py
@@ -8,8 +8,5 @@ else:
     urls = ['https://www.antmoe.com/']
 for i in range(0, len(urls)):
     req = requests.get(urls[i])
-    if i == 'https://www.antmoe.com/':
-        print('你没有正确添加地址哦！默认GET作者博客呢！')
-    else:
-        print(f'第{i+1}号网址唤醒状态:', req, time.strftime(
-            '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    print(f'第{i+1}号网址唤醒状态:', req, time.strftime(
+        '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
