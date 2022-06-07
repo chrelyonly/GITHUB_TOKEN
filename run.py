c1f6c80928faa68e94f13ddcfc7076e7421a201c
吃草莓的小屋 <62467038+chrelyonly@users.noreply.github.com>
2022-06-07 10:30:45 +0800

    Create run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import time
 if (len(sys.argv) >= 2):
     urls = sys.argv[1].split(',')
 else:
-    urls = ['https://www.antmoe.com/']
+    urls = ['https://www.chrelyonly.com/']
 for i in range(0, len(urls)):
     req = requests.get(urls[i])
     print(f'第{i}号网址唤醒状态:', req, time.strftime(
